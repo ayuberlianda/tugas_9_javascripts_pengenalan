7d74d24caa820e48d446c3af019a660bbd7f38b0
ayuberlianda <71421525+ayuberlianda@users.noreply.github.com>
2020-10-20 21:56:26 +0700

    Add files via upload

---
+++ tugas9.js
@@ -0,0 +1,8 @@
+var dataDiri={firstName:"Ayu", lastName:"Berlianda", age:29}
+var x
+
+console.log(dataDiri)
+console.log("Tampilkan menggunakan for in")
+for(x in dataDiri){
+	console.log(x, " : ",dataDiri[x])
+}
